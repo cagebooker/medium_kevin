dbb70a4ae30b76fc82d7206077fe0ca22481455c
KevinGu <cagebooker@gmail.com>
2022-10-22 18:55:44 +0800

    首頁內文章網址設定

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,11 @@
 class PagesController < ApplicationController
     def index
+        @stories = Story.order(created_at: :desc).includes(:user)
+    end
+    def show
+
+    end
+    def user
 
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :stories
   root "pages#index"
+
+  #首頁文章的連結 /@kevingu/文章-123
+  get '@:username/:story_id', to: 'pages#show', as: 'story_page'
+  #他人看作者的所有文章列表
+  get '@:username', to: 'pages#user', as: 'user_page'
+
 end
